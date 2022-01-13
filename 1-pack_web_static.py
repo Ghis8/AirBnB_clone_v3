d53274aaec8e330146c8b9910b5efbb4427b64e2
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-13 15:26:20 -0500

    [ADD] - Creation code and file task 1 0x03

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from fabric.api import *
+import datetime
+
+
+def do_pack():
+    name_file = "web_static_"
+    date = datetime.datetime.now()
+    split_v = str(date).split(' ')
+    first_v = split_v[0].split('-')
+    second_v = split_v[1].split(':')
+    name_file = name_file + first_v[0] + first_v[1] + first_v[2]
+    name_file = name_file + second_v[0] + second_v[1] + str(second_v[2][:2])
+    name_file = name_file + ".tgz"
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/{} web_static".format(name_file))
+    if result is None:
+        return None
+    else:
+        return name_file
