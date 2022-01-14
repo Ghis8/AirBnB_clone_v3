8b8fa8e7f8da41a8d149579b393c798480add9b3
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-14 17:27:38 -0500

    [UPD] - change logic

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,55 +1,64 @@
 #!/usr/bin/python3
-
+"""Fabric script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, using the function do_pack."""
 from fabric.api import *
-import datetime
-import os.path
-from fabric.operations import put, run
-
-env.hosts = ['34.138.80.184', '34.139.204.147']
+from datetime import datetime
+import os
 
 
 def do_pack():
-    """Create a file compress with information static"""
-    name_file = "web_static_"
-    date = datetime.datetime.now()
-    split_v = str(date).split(' ')
-    first_v = split_v[0].split('-')
-    second_v = split_v[1].split(':')
-    name_file = name_file + first_v[0] + first_v[1] + first_v[2]
-    name_file = name_file + second_v[0] + second_v[1] + str(second_v[2][:2])
-    name_file = name_file + ".tgz"
+    """Fabric script that generates a .tgz archive from the contents of
+    the web_static folder of your AirBnB Clone repo,
+    using the function do_pack."""
+
     local("mkdir -p versions")
-    result = local("tar -cvzf versions/{} web_static".format(name_file))
-    if result is None:
-        return None
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    tgz_file = "versions/web_static_{}.tgz".format(date)
+    execute_tar = local("tar -cvzf {} web_static".format(tgz_file))
+
+    if execute_tar.succeeded:
+        return tgz_file
     else:
-        return name_file
+        return None
+
+
+env.hosts = ['35.231.240.149', '54.167.32.11']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Upload files servers"""
-    if os.path.exists(archive_path):
-        try:
-            file_name = archive_path.split('/')[-1][:-4]
-            directory_s = "/data/web_static/releases/{}/".format(file_name)
-            put(archive_path, "/tmp/")
-            run("mkdir -p {}".format(directory_s))
-            run("tar -xzf /tmp/{}.tgz -C {}".format(file_name, directory_s))
-            run("rm -rf /tmp/{}.tgz".format(file_name))
-            run("mv {}/web_static/* {}".format(directory_s, directory_s))
-            run("rm -rf {}/web_static".format(directory_s))
-            run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ \
-/data/web_static/current".format(file_name))
-            return True
-        except:
-            return False
-    return False
+    """Fabric script that distributes an archive to your web servers,
+    using the function do_deploy"""
+    if os.path.isfile(archive_path) is False:
+        return False
+    else:
+        file_name = archive_path[9:]
+        path_to_uncompress = "/data/web_static/releases/" + file_name[:-4]
+        path_to_store = "/tmp/" + file_name
+
+        put(archive_path, "/tmp/")
+
+        run("sudo mkdir -p {}".format(path_to_uncompress))
+        run("sudo tar -xzf {} -C {}".format(path_to_store, path_to_uncompress))
+        run("sudo rm {}".format(path_to_store))
+        run("sudo mv {}/web_static/* {}".format(path_to_uncompress,
+                                                path_to_uncompress))
+        run("sudo rm -rf {}/web_static".format(path_to_uncompress))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current"
+            .format(path_to_uncompress))
+
+        print("New version deployed!")
+
+        return True
 
 
 def deploy():
-    """Create .tgz and load the file in servers"""
-    file_name = do_pack()
-    if file_name is None:
+    """Fabric script that creates and distributes an archive
+    to your web servers, using the function deploy"""
+
+    tgz_file = do_pack()
+    if tgz_file is None:
         return False
-    return do_deploy(file_name)
+    do_deploy_value = do_deploy(tgz_file)
+    return do_deploy_value
