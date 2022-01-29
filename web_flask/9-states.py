f0e8c6b95ea5696734371fc801f0baf28e9552c8
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-29 11:36:44 -0500

    [ADD] - Creation code and file task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module using flask to get routes"""
+
+from http.client import FOUND
+from flask import Flask, render_template
+from models import *
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """GET all information of route /states (Objects State)"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, id=False)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """Route /states with filter <id> (Objects State)"""
+    states = storage.all(State)
+    for state in states.values():
+        if state.id == str(id):
+            return render_template('9-states.html', states=state)
+    return render_template('9-states.html', no_found=True)
+
+
+@app.teardown_appcontex
+def storage_close(self):
+    '''Close current session'''
+    storage.close()
+
+"""Init"""
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+    )
